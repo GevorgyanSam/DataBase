a4143205bbd89a5c243f0ed61ec5c2983a2e2afc
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-10-02 19:54:24 +0300

    Fixing Some Grid Bugs

---
+++ section/nav.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="navParent">
+        <div class="navChild">
+            <h2><?=$Login?></h2>
+        </div>
+        <div class="navChild">
+            <a href="logout.php">
+            <div class="logout">
+                <h3>Log Out</h3>
+                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            </div>
+            </a>
+        </div>
+    </div>
+</nav>
